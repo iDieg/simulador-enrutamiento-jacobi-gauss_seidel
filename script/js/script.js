1cd0a59d7c64caa0ba619d9e1ef2f14ac2f4c742
iDieg <romerodiegoa08@gmail.com>
2026-03-25 09:18:17 -0400

    Update script/js/script.js

--- script/js/script.js
+++ script/js/script.js
@@ -1,6 +1,5 @@
     let n_actual = 0;
     let pasosGlobales = { jacobi: [], gauss: [] };
-    let randomNumber = Math.floor(Math.random() * 98 + 1);
 
     // Genera las casillas de input dependiendo de la cantidad de routers
     function generarCampos() {
@@ -38,12 +37,13 @@
         }
 
         for (let i = 0; i < n_actual; i++) {
-            let rowHTML = `<div class="row mb-2 align-items-center"><div class="col-2 fw-bold">Al Router ${i+1} (Ej. ${randomNumber}):</div>`;
+            let randomNumber = Math.floor(Math.random() * 98 + 1);
+            let rowHTML = `<div class="row mb-2 align-items-center"><div class="col-2 fw-bold">Al Router ${i+1}:</div>`;
             for (let j = 0; j < n_actual; j++) {
                 if (i !== j) {
                     rowHTML += `
                     <div class="col">
-                        <input type="number" class="form-control enlace-input" data-i="${i}" data-j="${j}" placeholder="Desde R${j+1} (%)" min="0" max="100">
+                        <input type="number" class="form-control enlace-input" data-i="${i}" data-j="${j}" placeholder="Desde R${j+1} (Ej. ${randomNumber})" min="0" max="100">
                     </div>`;
                 } else {
                     rowHTML += `<div class="col text-center text-muted">100</div>`;
